1e2c468151677012b113aa47caec5ad2b8df2b76
Bo Maryniuk <bo@maryniuk.net>
2020-04-02 15:36:59 +0200

    Update new clients lister

--- wzoperations.go
+++ wzoperations.go
@@ -1,10 +1,6 @@
 package whizz
 
 import (
-	"log"
-
-	"github.com/davecgh/go-spew/spew"
-
 	"github.com/infra-whizz/wzlib"
 	wzlib_transport "github.com/infra-whizz/wzlib/transport"
 )
@@ -13,10 +9,10 @@ import (
 func (wzc *WzClient) send(evp *wzlib_transport.WzGenericMessage) {
 	out, err := evp.Serialise()
 	if err != nil {
-		log.Println("Error serialising envelope:", err.Error())
+		wzc.GetLogger().Errorln("Error serialising envelope:", err.Error())
 	} else {
 		if err := wzc.transport.GetPublisher().Publish(wzlib.CHANNEL_CONSOLE, out); err != nil {
-			log.Println("Error sending message:", err.Error())
+			wzc.GetLogger().Errorln("Error sending message:", err.Error())
 		}
 	}
 }
@@ -30,17 +26,23 @@ func (wzc *WzClient) Accept(fingerprints ...string) {
 func (wzc *WzClient) Reject(fingerprints ...string) {
 }
 
-func (wzc *WzClient) ListNew() {
+// ListNew clients returning info about each client.
+func (wzc *WzClient) ListNew() []map[string]interface{} {
 	envelope := wzlib_transport.NewWzMessage(wzlib_transport.MSGTYPE_CLIENT)
 	envelope.Payload[wzlib_transport.PAYLOAD_COMMAND] = "list.clients.new"
 	wzc.send(envelope)
 	wzc.Wait(5)
-	log.Println("-- Begin reading")
-	for _, m := range wzc.replies {
-		log.Println(".........")
-		spew.Dump(m)
+
+	clients := make([]map[string]interface{}, 0)
+	for _, msg := range wzc.replies {
+		payload := msg.Payload[wzlib_transport.PAYLOAD_FUNC_RET].(map[string]interface{})["registered"]
+		if payload != nil {
+			for _, client := range payload.([]interface{}) {
+				clients = append(clients, client.(map[string]interface{}))
+			}
+		}
 	}
-	log.Println("-- End reading")
+	return clients
 }
 
 func (wzc *WzClient) ListRejected() {
