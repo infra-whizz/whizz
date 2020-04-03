e767be8995297dff1f8e5aa643d95551e2c0a071
Bo Maryniuk <bo@maryniuk.net>
2020-04-03 14:30:42 +0200

    Add simple formatter for systems listing

--- cli/stdfmt.go
+++ cli/stdfmt.go
@@ -3,6 +3,8 @@ package whizz_cli
 import (
 	"fmt"
 	"reflect"
+
+	"github.com/infra-whizz/wzlib"
 )
 
 type WhizzCliFormatter struct{}
@@ -31,4 +33,22 @@ func (wcf *WhizzCliFormatter) printMap(data map[string]interface{}, ident string
 // HostnameWithFp prints hostname and fingerprint underneath
 func (wcf *WhizzCliFormatter) HostnameWithFp(idx int, hostname string, fingerprint string) {
 	fmt.Printf("%d.  %s\n    +- %s\n", idx, hostname, fingerprint)
+}
+
+func (wcf *WhizzCliFormatter) ListSystems(systems []interface{}) {
+	statuses := map[int]string{
+		wzlib.CLIENT_STATUS_ACCEPTED: "Accepted",
+		wzlib.CLIENT_STATUS_NEW:      "New",
+		wzlib.CLIENT_STATUS_REJECTED: "Rejected",
+	}
+	if len(systems) > 0 {
+		fmt.Printf("%d machine(s) found:\n", len(systems))
+		for idx, system := range systems {
+			s := system.(map[string]interface{})
+			fmt.Printf("  %d.  %s\t%s\t%s\n", idx+1, s["RsaFp"].(string)[:8], statuses[int(s["Status"].(int64))], s["Fqdn"])
+		}
+	} else {
+		fmt.Println("No machines found by this criteria")
+	}
+
 }
