b2b06cef8bd8f3cc0c05d30fbc360fb90d205625
Bo Maryniuk <bo@maryniuk.net>
2020-05-12 21:00:11 +0200

    Add error message processing. THis should go to the wzlib/transport instead

--- wzevents.go
+++ wzevents.go
@@ -19,12 +19,28 @@ func NewWzEvents() *WzEvents {
 	return wze
 }
 
+// XXX: this should be a part of transport!
+func (wze *WzEvents) getMessageError(envelope *wzlib_transport.WzGenericMessage) string {
+	fRetBlock, ex := envelope.Payload[wzlib_transport.PAYLOAD_FUNC_RET]
+	if ex {
+		err, ex := fRetBlock.(map[string]interface{})["error"]
+		if ex {
+			return err.(string)
+		}
+	}
+	return ""
+}
+
 // onControllerReplyEvent function is fired when a controller has been replied
 func (wze *WzEvents) onControllerReplyEvent(msg *nats.Msg) {
 	envelope := wzlib_transport.NewWzGenericMessage()
 	if err := envelope.LoadBytes(msg.Data); err != nil {
 		wze.GetLogger().Errorln(err.Error())
 	} else {
+		if errmsg := wze.getMessageError(envelope); errmsg != "" {
+			wze.GetLogger().Errorf("Error: %s", errmsg)
+			return
+		}
 		batchMax, ok := envelope.Payload[wzlib_transport.PAYLOAD_BATCH_SIZE]
 		if !ok || batchMax == nil {
 			wze.GetLogger().Warningln("Discarding controller reply: no batch.max defined")
